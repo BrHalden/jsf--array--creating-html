7407ed2e013a2c217e11ca9412798561c879f07a
Kristoffer B. Halden <kris-b.h@hotmail.com>
2020-03-03 15:09:29 +0100

    Done and dusted

--- js/6-htmlfromapi.js
+++ js/6-htmlfromapi.js
@@ -2,6 +2,34 @@
 // The API returns a list of Rick and Morty characters.
 
 // 1. View the API in your browser to see the data you will recieve.
+
+fetch('https://rickandmortyapi.com/api/character/')
+  .then((response) => {
+    var myJSON = response.json();
+    return myJSON
+  })
+  .then((data) => {
+  	console.log(data.results)
+
+    var htmlString = data.results
+    	.filter(char => char.status === "Alive")
+    	.map(convertToHtml)
+    	.join("")
+
+    document.body.innerHTML += htmlString;
+  });
+function convertToHtml(data) {
+	return `
+		<div>
+			<img src="${data.image}" alt="data.name" />
+			<p>Character ID: ${data.id}</p>
+			<h3>Name: ${data.name}</h3>
+			<p>Character Species: ${data.species}</p>
+			<p>Status : ${data.status}</p>
+		</div>
+	`
+};
+
 //    The API returns and object with a property called results.  Results is an array of characters.
 // 2. Look at the properties/data on each character.
 // 3. Create a function called renderCharacter that will take in one of the character objects.
@@ -14,3 +42,28 @@
 // 6. Output the mapped HTML in the browser.
 // 
 // 7. BONUS TASK - use array.filter to only show characters that are alive.
+
+/*fetch('https://rickandmortyapi.com/api/character/')
+  .then((response) => {
+    var myJSON = response.json();
+    return myJSON
+  })
+  .then((data) => {
+    convertToHtml(data.results);
+    var htmlString = data.results.filter(char => char.status == "Alive").join("")
+    document.body.innerHTML += htmlString;
+  });
+function convertToHtml(data) {
+	return `
+		<div>
+			<img src="${data.image}" alt="data.name" />
+			<p>Character ID: ${data.id}</p>
+			<h3>Name: ${data.name}</h3>
+			<p>Character Species: ${data.species}</p>
+			<p>Status : ${data.status}</p>
+		</div>
+	`
+};
+*/
+
+
